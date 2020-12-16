905cde3de54884969da8ac8cf95e6ba95312bc5f
Joseph Gabriel <josegabriel.jgs18@gmail.com>
2020-12-15 23:05:50 -0300

    Criada a rota para salvar perguntas e recebendo os dados na mesma

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
@@ -10,6 +14,13 @@ app.get("/", (req, res) => {
 
 app.get("/perguntar", (req, res) => {
   res.render("perguntar");
+});
+
+app.post("/salvarpergunta", (req, res) => {
+  let titulo = req.body.titulo;
+  let descricao = req.body.descricao;
+
+  res.send("Formulario: " + titulo + " " + descricao);
 });
 
 const port = process.env.PORT || 8080;
